fd3b35e535173f531320d475c45a84f1a2075ee0
Odin <adeniyiotemade@gmail.com>
2019-09-17 14:04:25 +0100

    modified index.html, login.html and main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+/*
+var fadein = $('div.fadein');
+$.each(fadein, function(i, item) {
+	setTimeout(function() {
+		$(item).fadeIn(1000); // duration of fadein
+	}, 1000 * i); // duration between fadeins
+});​
+*/
