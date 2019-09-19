1fc76506026f39cfa9cc68014e73023e1914f17a
Odin <adeniyiotemade@gmail.com>
2019-09-19 04:24:49 +0100

    Updated the admin page for the blogger only

---
+++ admin.js
@@ -0,0 +1,15 @@
+var AddBlogView = Parse.View.extend({
+    template: Handlebars.compile($('#add-tpl').html()),
+    render: function(){
+        this.$el.html(this.template());
+    }
+});
+WelcomeView = Parse.View.extend({
+    template: Handlebars.compile($('#welcome-tpl').html()),
+    events: {
+        'click .add-blog': 'add'
+    },
+    add: function(){
+        alert('Clicked!');
+    },
+});
--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
-    $(".fadeIn").fadeIn(1000);
+    $("#password_error_message").hide();
+    var error_password = false;
+    
+    function check_password() {
+        var password_length = $("#form_password").val().length
+        if(password_length < 8) {
+            $("#password_error_message").html("Atleast 8 characters");
+            $("#password_error_message").show();
+            $("#form_password").css("border-bottom","2px solid #f90a0a");
+            error_password = true;
+        } else {
+            $("#password_error_message").hide();
+            $("form_password").css("border-bottom","2px solid #34f458");
+        }
+    }
 });
